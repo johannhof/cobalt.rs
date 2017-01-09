6ceef56aefab422ecbc0978b9aa143b5232c6d2a
Johann Hofmann <mail@johann-hofmann.com>
2017-01-09 12:41:05 +0100

    Hopefully fix repeated rustfmt installations

--- ci/install.sh
+++ ci/install.sh
@@ -16,16 +16,16 @@ main() {
            --force \
            --git japaric/cross \
            --tag v0.1.4 \
-           --target $target
+           --target $TARGET
 
     # Install rustfmt
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
+           --force \
            --crate rustfmt \
            --git japaric/rustfmt-bin \
            --tag v0.6.3-20170107 \
-           --target $TARGET \
-           --to ~/.cargo/bin
+           --target $TARGET
 
     if [ $TRAVIS_RUST_VERSION == nightly ]; then
       cargo install clippy || true
